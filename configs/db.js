25a969023b0e85b6b1c6dc4bce41b8ef16e2c2b4
Elahe <elafarahani10@gmail.com>
2026-06-11 01:51:14 -0700

    connect mongodb atlas

--- configs/db.js
+++ configs/db.js
@@ -5,7 +5,7 @@ const connectToDB =async ()=>{
         if(mongoose.connections[0].readyState){
         return true
     }else{
-await mongoose.connect("mongodb://localhost:27017/set-coffee")
+await mongoose.connect(process.env.MONGODB_URI)
 console.log('connect to DB duccessfully :))');
 
     } 
