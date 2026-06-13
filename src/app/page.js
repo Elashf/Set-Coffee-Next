5c1bd883f6f8bb832fd97cb6ccac855c8641dd03
Elahe <elafarahani10@gmail.com>
2026-06-12 23:58:21 -0700

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,20 @@ import Latest from "@/components/templates/index/latest/Latest";
 import Promote from "@/components/templates/index/promote/Promote";
 import connectToDB from "@/configs/db";
 import productModel from "@/models/Product";
+import { authUser } from "@/utils/authUser";
 
 
 
 export default async function Home() {
 await connectToDB()
+
   const latestProducts= await productModel.find({}).sort({_id:-1}).limit(8).lean()
-console.log("Products:", latestProducts.length);
+const user = await authUser()
+
 
   return (
     <>
-      <Navbar />
+      <Navbar isLogin={user? true : false}/>
       <Banner />
       <Latest products={JSON.parse(JSON.stringify(latestProducts))}/>
       <Promote />
